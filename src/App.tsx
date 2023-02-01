eb972f17c582b4ef9ea0083b798de60134309558
Bader Mohammad <bader97alyami@hotmail.com>
2023-02-01 21:25:49 +0300

    My home work

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Slider from './components/Slider';
 
 function App() {
 
 
   return (
     <div className="App">
-    
+      <Nav/>
+    <Slider/>
+     <Footer/>
     </div>
   )
 }
 
-export default App
+export default App;
